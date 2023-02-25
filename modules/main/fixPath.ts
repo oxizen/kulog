b1c9a2d129da69b7e3aaf22cd1161838348d2f34
Ben <oxizen@gmail.com>
2023-02-25 14:19:35 +0900

    🔨 skip path-fix on win32 platform

--- modules/main/fixPath.ts
+++ modules/main/fixPath.ts
@@ -6,6 +6,7 @@ const detectDefaultShell = () => {
 };
 
 const fix = () => new Promise<void>(resolve => {
+  if (process.platform === 'win32') return resolve();
   const shell = detectDefaultShell();
   exec(`${shell} -ilc env`, (e, out) => {
     const path = out.split('\n').find(row => row.startsWith('PATH='))?.split('=')[1];
